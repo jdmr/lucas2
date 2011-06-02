11957e71878fdd7e5b87cca97ba51b3f79054fec
Jairo Lopez <jairo.lopez@um.edu.mx>
2011-06-02 10:22:26 -0500

    se actualizo examenFisico

--- test/unit/app/TratamientoControllerTests.groovy
+++ test/unit/app/TratamientoControllerTests.groovy
@@ -1,17 +1,113 @@
 package app
 
-
-
-import grails.test.mixin.*
 import org.junit.*
+import grails.test.mixin.*
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(TratamientoController)
+@Mock(Tratamiento)
 class TratamientoControllerTests {
 
-    void testSomething() {
 
+    @Test
+    void indexDebieraHacerRedirectALista() {
+        controller.index()
+        assert "/tratamiento/lista" == response.redirectedUrl
     }
+
+    @Test
+    void debieraMostrarListaDeTratamientos() {
+        def tratamientos = []
+        for(i in 1..20) {
+            tratamientos << new Tratamiento(
+                descripcion: "TEST$i"
+            )
+        }
+        mockDomain(Tratamiento, tratamiento)
+
+        def model = controller.lista()
+
+        assert model.tratamientos.size() == 10
+        assert model.totalDeTratamientos == 20
+    }
+
+    @Test
+    void debieraCrearUnTratamiento() {
+        def model = controller.nuevo()
+        assert model.tratamiento != null
+
+        params.descripcion = 'TEST1'
+
+        controller.crea()
+        assert '/tratamiento/ver/1' == response.redirectedUrl
+    }
+
+    @Test
+    void debieraVerTratamiento() {
+        def tratamientos = []
+        for(i in 1..2) {
+            tratamientos << new Tratamiento(
+                id:i
+                ,version:1
+                ,descripcion: "TEST$i"
+                ,medicina: "TEST$i"
+                ,dosis: "TEST$i"
+                ,frecuencia: "TEST$i"
+            )
+        }
+        mockDomain(Tratamiento, tratamientos)
+
+        params.id = 1
+        def model = controller.ver()
+        assert model.tratamiento.descripcion == 'TEST1'
+    }
+
+    @Test
+    void debieraActualizarTratamiento() {
+        def tratamientos = []
+        for(i in 1..2) {
+            tratamientos << new Tratamiento(
+                id:i
+                ,version:1
+                ,descripcion: "TEST$i"
+                ,medicina: "TEST$i"
+                ,dosis: "TEST$i"
+                ,frecuencia: "TEST$i"
+            )
+        }
+        mockDomain(Tratamiento, tratamientos)
+
+        params.id = 1
+        def model = controller.edita()
+        assert model.tratamiento.descripcion == 'TEST1'
+
+        params.id = 1
+        params.descripcion = 'PRUEBA'
+        model = controller.actualiza()
+        assert '/tratamiento/ver/1' == response.redirectedUrl
+    }
+
+    @Test
+    void debieraEliminarTratamiento() {
+        def tratamientos = []
+        for(i in 1..2) {
+            tratamientos << new Tratamiento(
+                id:i
+                ,version:1
+                ,descripcion: "TEST$i"
+                ,medicina: "TEST$i"
+                ,dosis: "TEST$i"
+                ,frecuencia: "TEST$i"
+            )
+        }
+        mockDomain(Tratamiento, tratamientos)
+
+        params.id = 1
+        def model = controller.edita()
+        assert model.tratamiento.descripcion == 'TEST1'
+
+        params.id = 1
+        controller.elimina()
+        assert '/tratamiento/lista' == response.redirectedUrl
+    }
+
 }
